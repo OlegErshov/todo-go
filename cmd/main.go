cdb595462b9ef0e49469f2775cb0095c18a0bdc6
oleg <olezhka1629@gmail.com>
2024-06-11 21:49:57 +0300

    add achitecture skeleton

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 	todo_go "todo-go"
+	"todo-go/pkg/handler"
+	"todo-go/pkg/repository"
+	"todo-go/pkg/service"
 )
 
 func main() {
+	repositories := repository.NewRepository()
+	services := service.NewService(repositories)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo_go.Server)
-	if err := srv.Start("8000"); err != nil {
+	if err := srv.Start("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Erorr in attempting to start server: %v", err.Error)
 	}
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,1 +1,46 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"todo-go/pkg/service"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-in", h.SignIn)
+		auth.POST("/sign-up", h.SignUp)
+	}
+
+	api := router.Group("/api")
+	{
+		lists := router.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			items := api.Group("/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+				items.GET("/:id", h.getItemById)
+				items.PUT("/:id", h.updateItem)
+				items.DELETE("/:id", h.deleteItem)
+			}
+		}
+	}
+	return router
+}
